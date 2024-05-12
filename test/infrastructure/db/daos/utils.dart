7c14953f83ae82484b43f6176231079564c8c802
Jakelin Correa <jakelin@unergy.io>
2024-05-12 17:55:04 -0500

    Eliminados tests (Porque están desactualizados)

--- test/infrastructure/db/daos/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:drift/drift.dart';
-import 'package:inspecciones/infrastructure/drift_database.dart';
-
-Future<int?> getNroFilas<T extends HasResultSet, R>(
-    Database db, ResultSetImplementation<T, R> t) {
-  final count = countAll();
-  final query = db.selectOnly(t)..addColumns([count]);
-  return query.map((row) => row.read(count)).getSingle();
-}
